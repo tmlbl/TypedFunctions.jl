8361639455b4a9f8fba0b765e6a679e5a0605bac
Tim Lebel <tim@corporation.enterprises>
2016-08-24 21:22:57 -0700

    Still figuring out multiple return types

--- src/TypedFunctions.jl
+++ src/TypedFunctions.jl
@@ -1,21 +1,57 @@
+isdefined(:__precompile__) && __precompile__()
+
 module TypedFunctions
 
-export @typed
+export @typed,
+       TypedFunction
 
 import Base.call,
-       Base.Func
+       Base.Func,
+       Base.return_types
 
 type TypedFunction <: Func
     f::Function
-    rtype::Type
+    sig::Vector{DataType}
+    rtype::Vector{DataType}
 end
 
-call(tf::TypedFunction, x...) = call(tf.f, x...)
+function TypedFunction(f, s, r)
+    @show s 
+    @show r
+end
+
+# TypedFunction(f::Function, r::Type) = TypedFunction(f, [r])
+
+function call(tf::TypedFunction, x...)
+    for (i, typ) in enumerate(return_types(tf.f, tf.sig))
+        @assert typ <: tf.rtype[i] "Expected return value $i to be of type $(tf.rtype[i])"
+    end
+    tf.f(x...)
+end
+
+function getsignature(fun::Expr)
+    types = Symbol[]
+    for param in fun.args[1].args[2:end]
+        push!(types, param.args[2])
+    end
+    types
+end
 
 macro typed(func)
-    @show typeof(func.args[2].args[2].args[1])
+    # Create a private name for the underlying function
+    fname = func.args[1].args[1]
+    fprivate = symbol("__$fname")
+    func.args[1].args[1] = fprivate
+    # Get the type annotation
+    typeAnno = func.args[2].args[2].args[1]
+    # Get the function signature 
+    fsig = getsignature(func)
     return quote
+        typ = $(esc(typeAnno))
+        typ = typeof(typ) <: AbstractVector ? typ : [typ]
+        sig = $(esc(map(eval, fsig)))
         $(esc(func))
+        $(esc(fname)) = TypedFunction($(esc(fprivate)), sig, typ)
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,17 @@
 using TypedFunctions,
       Base.Test
 
-@typed function add(x::Real, y::Real): Real 
+@typed function add(x::Int64, y::Int64): Int64 
     x + y
 end
 
 @test add(2, 3) == 5
+@test typeof(add(2, 3)) <: Int64
+@test_throws MethodError add("foo")
+
+println("Multiple return types")
+@typed function noot(): (Bool, Bool)
+    return true, false
+end
+
+@test noot() == (true, false)
