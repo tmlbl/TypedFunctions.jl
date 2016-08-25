4981f1b2e0e83277e8a27776f836eb98664c3a0d
Tim Lebel <tim.lebel@multiscalehn.com>
2016-08-24 17:10:31 -0700

    Starting on something

--- src/TypedFunctions.jl
+++ src/TypedFunctions.jl
@@ -2,6 +2,16 @@ module TypedFunctions
 
 export @typed
 
+import Base.call,
+       Base.Func
+
+type TypedFunction <: Func
+    f::Function
+    rtype::Type
+end
+
+call(tf::TypedFunction, x...) = call(tf.f, x...)
+
 macro typed(func)
     @show typeof(func.args[2].args[2].args[1])
     return quote
